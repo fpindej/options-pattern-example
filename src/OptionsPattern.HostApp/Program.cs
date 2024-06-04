f46cd08ef63113f9977dd064174f0c7714279d97
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-06-05 00:40:18 +0200

    Register Options to DI container

--- src/OptionsPattern.HostApp/Program.cs
+++ src/OptionsPattern.HostApp/Program.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using OptionsPattern.HostApp;
 
 var builder = Host.CreateDefaultBuilder(args);
 
 builder.ConfigureAppConfiguration(cfg =>
 {
     cfg.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+}).ConfigureServices(services =>
+{
+    services.AddEuropeanBankApiClientConfiguration();
 });
 
 var app = builder.Build();
