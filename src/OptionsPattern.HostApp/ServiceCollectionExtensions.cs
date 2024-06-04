35443f8942be18c67ad29f36b8983afa8ad4f8d2
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-06-05 00:38:17 +0200

    Create service registration method

---
+++ src/OptionsPattern.HostApp/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace OptionsPattern.HostApp;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddEuropeanBankApiClientConfiguration(this IServiceCollection services)
+    {
+        services.AddOptions<EuropeanBankApiClientConfiguration>()
+            .BindConfiguration(EuropeanBankApiClientConfiguration.SectionName)
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        return services;
+    }
+}
