f90d9264ff87fa7836351c5008115369122df669
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-06-05 00:22:48 +0200

    Add example configuration object

---
+++ src/OptionsPattern.HostApp/EuropeanBankApiClientConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OptionsPattern.HostApp;
+
+public sealed class EuropeanBankApiClientConfiguration
+{
+    public const string SectionName = "EuropeanBankApiClient";
+
+    [Required]
+    public Uri BaseUrl { get; init; }
+
+    public string DefaultCurrency { get; init; } = "EUR";
+
+    public RetryPolicy RetryPolicy { get; init; } = new();
+}
+
+public sealed class RetryPolicy
+{
+    public int MaxRetries { get; init; } = 3;
+
+    public TimeSpan Timeout { get; init; } = TimeSpan.FromSeconds(value: 5);
+}
