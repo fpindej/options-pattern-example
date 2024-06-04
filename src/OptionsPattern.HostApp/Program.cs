1650cb7de10c800f641c8fd0c805ef9d12636ad3
Filip Dorian Pindej <fpindej@users.noreply.github.com>
2024-06-05 00:46:25 +0200

    Show loaded appsettings via options pattern

--- src/OptionsPattern.HostApp/Program.cs
+++ src/OptionsPattern.HostApp/Program.cs
@@ -1,15 +1,22 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using OptionsPattern.HostApp;
 
 var builder = Host.CreateDefaultBuilder(args);
 
-builder.ConfigureAppConfiguration(cfg =>
-{
-    cfg.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-}).ConfigureServices(services =>
-{
-    services.AddEuropeanBankApiClientConfiguration();
-});
+builder
+    .ConfigureAppConfiguration(cfg => { cfg.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true); })
+    .ConfigureServices(services => { services.AddEuropeanBankApiClientConfiguration(); });
 
 var app = builder.Build();
+
+var europeanBankApiClientConfiguration =
+    app.Services.GetRequiredService<IOptions<EuropeanBankApiClientConfiguration>>().Value ??
+    throw new InvalidOperationException("EuropeanBankApiClientConfiguration is null");
+
+Console.WriteLine($"BaseUrl: {europeanBankApiClientConfiguration.BaseUrl}");
+Console.WriteLine($"DefaultCurrency: {europeanBankApiClientConfiguration.DefaultCurrency}");
+Console.WriteLine($"MaxRetries: {europeanBankApiClientConfiguration.RetryPolicy.MaxRetries}");
+Console.WriteLine($"Timeout: {europeanBankApiClientConfiguration.RetryPolicy.Timeout}");
